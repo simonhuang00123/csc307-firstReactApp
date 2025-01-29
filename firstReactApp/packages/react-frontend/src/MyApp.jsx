f94575792b7866e0ddab6e5cc0c815b533a40ff7
simonjhuang <sjhuangsf@gmail.com>
2025-01-29 12:42:49 -0800

    setup

--- firstReactApp/packages/react-frontend/src/MyApp.jsx
+++ firstReactApp/packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,5 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -14,7 +14,37 @@ import Form from "./Form";
     }
 
     function updateList(person) {
-        setCharacters([...characters, person]);
+      postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    function fetchUsers() {
+      const promise = fetch("http://localhost:8000/users");
+      return promise;
+    }
+
+    useEffect(() => {
+      fetchUsers()
+        .then((res) => res.json())
+        .then((json) => setCharacters(json["users_list"]))
+        .catch((error) => {
+          console.log(error);
+        });
+    }, []);
+
+    function postUser(person) {
+      const promise = fetch("Http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(person)
+      });
+    
+      return promise;
     }
 
       return (
